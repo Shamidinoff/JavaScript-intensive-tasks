7ff155352507273686d1eb3a2b99eb01940f024e
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-04-12 19:51:00 +0600

    Add files via upload

---
+++ 02-love-generator/start/js.js
@@ -0,0 +1,8 @@
+const firstName = prompt("Напишите первое имя: ");
+const secondName = prompt("Напишите второе имя: ");
+
+const randomNumber = Math.floor(math.random() * 100);
+
+alert(
+  `Результат совместимости имен ${firstName} и ${secondName} равен ${randomNumber}`
+);
